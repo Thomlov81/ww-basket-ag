f8a36aa62d7f907ba74c8037f16c2825b9f73c64
Thomas Løvdahl <thomas@skiltogreklame.no>
2026-03-11 09:41:13 +0000

    Fix: Stale row context in Tree Group column's GroupCellRenderer

--- src/components/GroupCellRenderer.vue
+++ src/components/GroupCellRenderer.vue
@@ -72,7 +72,7 @@ export default {
         syncFromParams() {
             this.expanded = this.params?.node?.expanded ?? false;
             this.currentValue = this.params?.value;
-            this.currentData = this.params?.data;
+            this.currentData = this.params?.data ? { ...this.params.data } : null;
             this.currentIsGroup = this.params?.node?.group ?? false;
             this.currentLevel = this.params?.node?.level ?? 0;
             this.currentChildCount = this.params?.node?.childrenAfterGroup?.length ?? 0;
