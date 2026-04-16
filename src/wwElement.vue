35854b2fa393b1507aacbd35b0e78a1e76f6cda1
Thomas Løvdahl <thomas@skiltogreklame.no>
2026-04-16 16:46:05 +0000

    Feat: Add RichSelectModule to AG Grid module registry

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -91,7 +91,7 @@ import {
   ModuleRegistry,
   themeQuartz,
 } from "ag-grid-community";
-import { TreeDataModule, LicenseManager } from "ag-grid-enterprise";
+import { TreeDataModule, RichSelectModule, LicenseManager } from "ag-grid-enterprise";
 import {
   AG_GRID_LOCALE_EN,
   AG_GRID_LOCALE_FR,
@@ -110,7 +110,7 @@ import GroupCellRenderer from "./components/GroupCellRenderer.vue";
 
 LicenseManager.setLicenseKey("Using_this_{AG_Grid}_Enterprise_key_{AG-123489}_in_excess_of_the_licence_granted_is_not_permitted___Please_report_misuse_to_legal@ag-grid.com___For_help_with_changing_this_key_please_contact_info@ag-grid.com___{Ernestus_Skilt_og_Reklame_AS}_is_granted_a_{Single_Application}_Developer_License_for_the_application_{Ernestus_Portal}_only_for_{1}_Front-End_JavaScript_developer___All_Front-End_JavaScript_developers_working_on_{Ernestus_Portal}_need_to_be_licensed___{Ernestus_Portal}_has_not_been_granted_a_Deployment_License_Add-on___This_key_works_with_{AG_Grid}_Enterprise_versions_released_before_{13_March_2027}____[v3]_[01]_MTgwNDg5NjAwMDAwMA==2418b1c6e811e23619cd7bc958129240");
 
-ModuleRegistry.registerModules([AllCommunityModule, TreeDataModule]);
+ModuleRegistry.registerModules([AllCommunityModule, TreeDataModule, RichSelectModule]);
 
 export default {
   components: {
